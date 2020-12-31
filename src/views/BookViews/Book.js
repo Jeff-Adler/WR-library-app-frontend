9961778e37c5c87ceb30fcff3e00209bfe70ed0b
Jeff-Adler <Jeffm.adler@gmail.com>
2020-12-31 15:48:36 -0500

    added modal to book to alt submission

--- src/views/BookViews/Book.js
+++ src/views/BookViews/Book.js
@@ -1,12 +1,14 @@
 import React, {useState} from 'react';
 import { Link, useParams, useHistory } from 'react-router-dom';
-import {ListGroup, ListGroupItem, Button } from 'reactstrap'
+import {ListGroup, ListGroupItem, Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap'
 
 export default function Book(props) {
     let history = useHistory();
     const [displayReferences, setDisplayReferences] = useState(false)
     const [selectedReference, setSelectedReference] = useState()
     const [isLoading, setIsLoading] = useState(false)
+    const [modal, setModal] = useState(false);
+    const toggle = () => setModal(!modal);
 
     let { bookId } = useParams();
     const {books, convertBookToAlt} = props  
@@ -47,6 +49,7 @@ export default function Book(props) {
     const bookConvertHandler = (e) => {
         e.preventDefault()
         if (selectedReference) {
+            toggle()
             setIsLoading(!isLoading)
             convertBookToAlt(bookId, selectedReference)
         } else {
@@ -86,9 +89,18 @@ export default function Book(props) {
                         <select name="cars" id="cars" onChange={e => {setSelectedReference(e.target.value)}}>
                             {mapReferences()}
                         </select><br/><br/>
-                        <Button onClick={bookConvertHandler}>
+                        <Button onClick={toggle}>
                             Submit
                         </Button>
+                        <Modal isOpen={modal} toggle={toggle} className="Modal">
+                        <ModalHeader toggle={toggle}>Submit Confirmation</ModalHeader>
+                        <ModalBody>{`Are you sure you want to label ${book.title} as a duplicate?`}
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button color="primary" onClick={bookConvertHandler}>Continue</Button>{' '}
+                            <Button color="secondary" onClick={toggle}>Cancel</Button>
+                        </ModalFooter>
+                    </Modal>
                     </div>
                     : 
                     ""

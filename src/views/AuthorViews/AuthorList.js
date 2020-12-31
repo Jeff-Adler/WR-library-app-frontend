7eafa448e46d4a5bd42a9d83dd2fbf0b12ac7145
Jeff-Adler <Jeffm.adler@gmail.com>
2020-12-31 16:21:32 -0500

    added books to author_list

--- src/views/AuthorViews/AuthorList.js
+++ src/views/AuthorViews/AuthorList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link, useRouteMatch } from 'react-router-dom';
-import { ListGroup, ListGroupItem } from 'reactstrap';
+import { ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText  } from 'reactstrap';
 
 export default function AuthorList (props) {
     let { path } = useRouteMatch();
@@ -9,9 +9,25 @@ export default function AuthorList (props) {
     const mapAuthors = () => {
         return authors.map((author) => {
             return (
-                <ListGroupItem key={`author_${author.id}`} tag={Link} to={`${path}/${author.id}`}>
-                    {`${author.first_name} ${author.last_name}`}
+                <ListGroupItem>
+                    <ListGroupItemHeading key={`author_${author.id}`} tag={Link} to={`${path}/${author.id}`}>{`${author.first_name} ${author.last_name}`}</ListGroupItemHeading>
+                    <br/><br/>
+                    <ListGroupItemText>
+                    <strong>Books: </strong> {mapBooks(author)}
+                    </ListGroupItemText>
                 </ListGroupItem>
+            )
+        })
+    }
+
+    const mapBooks = (author) => {
+        return author.books.map((book) => {
+            return (
+                <Link to={`books/${book.id}`}>
+                    <span key={`book_${book.id}`}>
+                        {`${book.title}`}{`\xa0`}{`\xa0`}
+                    </span>
+                </Link>
             )
         })
     }

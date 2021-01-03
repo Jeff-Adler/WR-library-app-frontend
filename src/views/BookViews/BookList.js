f425a40e76dd4bdbe65a873d5c1a881b0da03c45
Jeff-Adler <Jeffm.adler@gmail.com>
2021-01-03 09:52:46 -0500

    added filter to books

--- src/views/BookViews/BookList.js
+++ src/views/BookViews/BookList.js
@@ -1,28 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link, useRouteMatch } from 'react-router-dom';
 import { ListGroup, ListGroupItem, ListGroupItemHeading, ListGroupItemText } from 'reactstrap';
 
 export default function BookList (props) {
     let { path } = useRouteMatch();
     const { books } = props
+    const [filter,setFilter] = useState("")
 
     const mapBooks = () => {
         return books.map((book) => {
-            return (
-                <ListGroupItem key={`book_${book.id}`}>
-                    <ListGroupItemHeading key={`book_${book.id}_title`} tag={Link} to={`${path}/${book.id}`}>{book.title}</ListGroupItemHeading>
-                    <br/><br/>
-                    <ListGroupItemText key={`book_${book.id}_duplicates`}>
-                    <strong>Duplicates: </strong> {mapAlts(book)}
-                    </ListGroupItemText>
-                </ListGroupItem>
-            )
+            if (book.title.toLowerCase().includes(filter.toLowerCase())) {
+                return (
+                    <ListGroupItem key={`book_${book.id}`}>
+                        <ListGroupItemHeading key={`book_${book.id}_title`} tag={Link} to={`${path}/${book.id}`}>{book.title}</ListGroupItemHeading>
+                        <br/><br/>
+                        <ListGroupItemText key={`book_${book.id}_duplicates`}>
+                        <strong>Duplicates: </strong> {mapAlts(book)}
+                        </ListGroupItemText>
+                    </ListGroupItem>
+                )
+            } else {
+                return ""
+            }
         })
     }
 
     const mapAlts = (book) => {
         return book.alts.map((alt) => {
-            return (
+            return (      
                 <Link key={`alt_${alt.id}`} to={`${path}/${book.id}/alts/${alt.id}`}>
                     <span>
                         {`${alt.title}`}{`\xa0`}{`\xa0`}
@@ -34,6 +39,8 @@ export default function BookList (props) {
 
     return (
         <div className="list-container"> 
+            <input name="books filter" className="filter" id="books-filter" placeholder="filter books" value={filter} onChange={(e) => setFilter(e.target.value)}/>
+            <br/><br/>
             <ListGroup>
                 {mapBooks()}  
             </ListGroup>
